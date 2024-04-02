d6574a9e1f4e99566a9fecfdb93b28e6ecd31bce
Bhekisile <bhekisilejozi@gmail.com>
2024-04-02 10:30:11 +0200

    Create class App with input for weather display

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: "Lisbon" };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <div>
+          <input 
+            type="text"
+            placeholder="Search from location..."
+            value={this.state.location}
+            // onChange={}
+          />
+        </div>
+        <button>Get weather</button>
+      </div>
+    );
+  }
+}
+
+export default App;
