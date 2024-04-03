d05e4c146641d9924dcdf7da586035c8cf03b583
Bhekisile <bhekisilejozi@gmail.com>
2024-04-03 13:52:40 +0200

    Implement lifecycle method for the mount and unmount

--- src/App.js
+++ src/App.js
@@ -60,13 +60,15 @@ async function getWeather(location) {
 
 class App extends React.Component {
   state = {
-    location: "Lisbon", 
+    location: "", 
     isLoading: false,
     displayLocation: "",
     weather: {},
   };
 
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {}}); 
+
     try {
       // We can mutate the state here but not with useState()
       this.setState({ isLoading: true });
@@ -91,7 +93,7 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
@@ -101,13 +103,16 @@ class App extends React.Component {
 
   // useEffect []
   componentDidMount() {
-    this.fetchWeather();
+    // this.fetchWeather();
+    this.setState({ location: localStorage.getItem("location") || ""});
   }
 
   // useEffect [location]
   componentDidUpdate(prevProps, prevState) {
     if (this.state.location !== prevState.location) {
       this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
     }
   }
 
@@ -116,7 +121,6 @@ class App extends React.Component {
       <div className="app">
         <h1>Classy Weather</h1>
         <Input location={this.state.location} onChangeLocation={this.setLocation} />
-        <button onClick={this.fetchWeather}>Get weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
         {this.state.weather.weathercode && (
